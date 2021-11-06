b52882627c082be9908c08ea3771e63879095cef
DyatlovSergey <s.dyatlov90@gmail.com>
2021-11-06 16:39:09 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('.ingredients');
+
+const elements = ingredients.map(ingredient => {
+const newItemEl = document.createElement('li');
+newItemEl.classList.add('item')
+newItemEl.textContent = ingredient;
+  return newItemEl;
+
+});
+
+console.log(elements);
+
+listEl.append(...elements);
+
+
+
+
+
+
